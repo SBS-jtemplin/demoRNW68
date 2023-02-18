799f004139de5a8d632926598caee5a90148d1b7
SBS-jtemplin <jtemplin@seabird.com>
2023-02-17 16:32:40 -0800

    add buttons to create and update plot

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@
  * @format
  */
 
-import React, { useRef, useCallback } from 'react';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import { Button, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Plotly from './lib/PlotlyBasic';
 
@@ -20,28 +20,21 @@ const html = `
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <style> #hello {background-color: red;} </style>
         </head>
         <body>
-            <p id="hello">Hello WebView</p>
             <div id="chart"></div>
         </body>
     </html>
 `
 
-const javascript = `
-    console.log('injectJavaScript');
-    document.getElementById('hello').style.backgroundColor = 'green';
-    true;
-`
-var data: any = [{
+let data: any = [{
     x: [1999, 2000, 2001, 2002],
     y: [10, 15, 13, 17],
     type: 'scatter'
 }];
 
 
-var layout: any = {
+const layout: any = {
     title: 'Sales Growth',
     xaxis: {
         title: 'Year',
@@ -51,22 +44,21 @@ var layout: any = {
     yaxis: {
         title: 'Percent',
         showline: false
-    }
+    },
+    width: 800,
+    height: 500
 };
 
 
 const App = () => {
     const webViewRef = useRef<WebView>(null);
 
-    
     const invoke = (str: string) => {
-        webViewRef?.current?.injectJavaScript(`(function(){${str}})()`);
+        if (webViewRef && webViewRef.current){
+            webViewRef?.current?.injectJavaScript(str);
+        }
     };
-    
-    const invokeEncoded = (str: string) => {
-        invoke(`eval(atob("${str}"));`);
-    };
-    
+
     const createPlot = (data: any, layout?: any, config?: any) => {
         invoke(`
             window.Plotly.newPlot(
@@ -74,27 +66,44 @@ const App = () => {
                 ${JSON.stringify(data)},
                 ${JSON.stringify(layout)},
                 ${JSON.stringify(config)}
-                ).then(function() {
-                    window.ReactNativeWebView.postMessage('Chart Loaded');
-                });
+                );
             `);
         };
-        
-    const handleLoadEnd = useCallback(() => {
-        console.log("handleLoadEnd");
-        webViewRef?.current?.injectJavaScript(javascript);
-        invokeEncoded(Plotly);
-        createPlot(data, layout);
-    },[])
+    
+    const updatePlot = (data: any, layout?: any, config?: any) => {
+        let newData = data;
+        newData[0].x.push(2003);
+        newData[0].y.push(14);
+        invoke(`
+            window.Plotly.react(
+                'chart',
+                ${JSON.stringify(data)},
+                ${JSON.stringify(layout)},
+                ${JSON.stringify(config)}
+                );
+            `);
+        };
 
     return (
         <View style={{flex: 1}}>
-            <WebView
-                ref={webViewRef}
-                source={{html: html}}
-                onLoadEnd={handleLoadEnd}
-                onMessage={() => {}}
-            />
+            <View style={{flexDirection: 'row', alignItems: 'stretch'}}>
+                <Button
+                    onPress={() => {createPlot(data, layout)}}
+                    title="Create Plot"
+                />
+                <Button
+                    onPress={() => {updatePlot(data, layout)}}
+                    title="Update Plot"
+                />
+            </View>
+            <View style={{flex: 1}}>
+                <WebView
+                    ref={webViewRef}
+                    source={{html: html}}
+                    onLoadEnd={() => { invoke(`eval(atob("${Plotly}"));`); }}
+                    onMessage={() => {}}
+                />
+            </View>
         </View>
     );
 }
