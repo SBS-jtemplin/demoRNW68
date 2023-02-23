3cc1d3d5244b66b7061968fe99dc678696022f38
SBS-jtemplin <jtemplin@seabird.com>
2023-02-22 17:17:28 -0800

    import plotly from data viz (BROKEN)

--- App.tsx
+++ App.tsx
@@ -1,31 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, { ReactElement } from 'react';
+import { PlotlyWebView } from '@sea-birdscientific/typescript-data-visualization/dist/DataVisualization'
 
-import React, { useRef } from 'react';
-import { Button, View } from 'react-native';
-import { WebView } from 'react-native-webview';
-import Plotly from './lib/PlotlyBasic';
-
-
-const html = `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-        </head>
-        <body>
-            <div id="chart"></div>
-        </body>
-    </html>
-`
 
 let data: any = [{
     x: [1999, 2000, 2001, 2002],
@@ -50,61 +25,13 @@ const layout: any = {
 };
 
 
-const App = () => {
-    const webViewRef = useRef<WebView>(null);
-
-    const invoke = (str: string) => {
-        if (webViewRef && webViewRef.current){
-            webViewRef.current.injectJavaScript(str);
-        }
-    };
-
-    const createPlot = (data: any, layout?: any, config?: any) => {
-        invoke(`
-            window.Plotly.newPlot(
-                'chart',
-                ${JSON.stringify(data)},
-                ${JSON.stringify(layout)},
-                ${JSON.stringify(config)}
-                );
-            `);
-        };
-    
-    const updatePlot = (data: any, layout?: any, config?: any) => {
-        let newData = data;
-        newData[0].x.push(2003);
-        newData[0].y.push(14);
-        invoke(`
-            window.Plotly.react(
-                'chart',
-                ${JSON.stringify(data)},
-                ${JSON.stringify(layout)},
-                ${JSON.stringify(config)}
-                );
-            `);
-        };
-
+const App = (): ReactElement => {
     return (
-        <View style={{flex: 1}}>
-            <View style={{flexDirection: 'row', alignItems: 'stretch'}}>
-                <Button
-                    onPress={() => {createPlot(data, layout)}}
-                    title="Create Plot"
-                />
-                <Button
-                    onPress={() => {updatePlot(data, layout)}}
-                    title="Update Plot"
-                />
-            </View>
-            <View style={{flex: 1}}>
-                <WebView
-                    ref={webViewRef}
-                    source={{html: html}}
-                    onLoadEnd={() => { invoke(`eval(atob("${Plotly}"));`); }}
-                    onMessage={() => {}}
-                />
-            </View>
-        </View>
+        // <></>
+        <PlotlyWebView
+            // data={data}
+            // layout={layout}
+        />
     );
 }
 
